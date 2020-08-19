07e9d1b8b47da5492fa939aa124c77c9e70dad28
unknown <luka.patrun@gmail.com>
2020-08-19 09:56:53 +0200

    imporved app

---
+++ dist/precache-manifest.aea07b7e17d18f2ac2f4a267ec66be1d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdfe47b31e9184a55cf02eef1baf7240",
+    "url": "/Azonix.otf"
+  },
+  {
+    "revision": "23bd234bfbf4b18f7234",
+    "url": "/css/app.38d5a174.css"
+  },
+  {
+    "revision": "a011af06cd7284e4d447",
+    "url": "/css/chunk-32f3c438.40b78a61.css"
+  },
+  {
+    "revision": "5af64fbeb7c4acad63f120de60f7f620",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "cdfe47b31e9184a55cf02eef1baf7240",
+    "url": "/fonts/Azonix.cdfe47b3.otf"
+  },
+  {
+    "revision": "338b73d1ca689f21664ab5ed7e405040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23bd234bfbf4b18f7234",
+    "url": "/js/app.91a81217.js"
+  },
+  {
+    "revision": "a011af06cd7284e4d447",
+    "url": "/js/chunk-32f3c438.907a5724.js"
+  },
+  {
+    "revision": "0b9b75a7c72fcb9c12b7",
+    "url": "/js/chunk-vendors.3f6b7d2a.js"
+  },
+  {
+    "revision": "535b9013f9319c8c4495f2fe3f4d345d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
