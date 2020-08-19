a075b0d0d0b686953a5fef1872ae2aabd2f0d1a2
unknown <luka.patrun@gmail.com>
2020-08-19 13:08:57 +0200

    base url fix15

---
+++ dist/precache-manifest.2c0b69427784b18cd7abeae37446aaaa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdfe47b31e9184a55cf02eef1baf7240",
+    "url": "/moviesVueJs/Azonix.otf"
+  },
+  {
+    "revision": "36d9e2f3fa9e4f989730",
+    "url": "/moviesVueJs/css/app.38d5a174.css"
+  },
+  {
+    "revision": "a011af06cd7284e4d447",
+    "url": "/moviesVueJs/css/chunk-32f3c438.40b78a61.css"
+  },
+  {
+    "revision": "5af64fbeb7c4acad63f120de60f7f620",
+    "url": "/moviesVueJs/favicon.png"
+  },
+  {
+    "revision": "cdfe47b31e9184a55cf02eef1baf7240",
+    "url": "/moviesVueJs/fonts/Azonix.cdfe47b3.otf"
+  },
+  {
+    "revision": "060fc5844125f056e74869c5c79f5093",
+    "url": "/moviesVueJs/index.html"
+  },
+  {
+    "revision": "36d9e2f3fa9e4f989730",
+    "url": "/moviesVueJs/js/app.2a66079b.js"
+  },
+  {
+    "revision": "a011af06cd7284e4d447",
+    "url": "/moviesVueJs/js/chunk-32f3c438.907a5724.js"
+  },
+  {
+    "revision": "755676ce52971f3c4319",
+    "url": "/moviesVueJs/js/chunk-vendors.e9508eb8.js"
+  },
+  {
+    "revision": "535b9013f9319c8c4495f2fe3f4d345d",
+    "url": "/moviesVueJs/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/moviesVueJs/robots.txt"
+  }
+]);
